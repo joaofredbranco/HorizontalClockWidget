4d7215368fa17b3ef380ed366ff09705fab0984b
joaof <joaofredbranco@gmail.com>
2024-06-12 10:31:38 +0100

    Commit

--- app/src/main/java/com/example/horizontalclockwidget/WidgetConfigActivity.kt
+++ app/src/main/java/com/example/horizontalclockwidget/WidgetConfigActivity.kt
@@ -69,6 +69,9 @@ class WidgetConfigActivity : Activity() {
                 "Red" -> Color.RED
                 "Green" -> Color.GREEN
                 "Blue" -> Color.BLUE
+                "Black" -> Color.BLACK
+                "White" -> Color.WHITE
+                "Yellow" -> Color.YELLOW
                 else -> Color.BLACK
             }
             val startTime = startTimeSpinner.selectedItem.toString().split(":")[0].toInt()
